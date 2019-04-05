b758cd7230c9b1704e0bd39fb50f36fef6944ec6
kostya <kostya@email.com>
2019-04-05 16:29:41 +0300

    Refactor HW7

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -42,13 +42,6 @@ class RootControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("meals"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals",
-                        new AssertionMatcher<List<MealTo>>() {
-                            @Override
-                            public void assertion(List<MealTo> actual) throws AssertionError {
-                                MealTestData.assertMatch(actual, MEALS_TO);
-                            }
-                        }
-                ));
+                .andExpect(model().attribute("meals", MEALS_TO));
     }
 }
